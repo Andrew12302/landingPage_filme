c9ef86185f8f946a5db8ab32c1be49613149b2c1
bruno <camposandrew950@gmail.com>
2025-09-03 12:15:02 -0300

    Finalização do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,33 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+// SCSS
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+// Imagens
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
+
+// JS
+function scripts() {
+    return gulp.src('./main.js')      // main.js na raiz
+        .pipe(gulp.dest('./dist/js')); // copia para dist/js
+}
+
+// Task padrão: roda SCSS + imagens + JS
+exports.default = gulp.parallel(styles, images, scripts);
+
+// Watch: observa mudanças em SCSS e JS
+exports.watch = function(){
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./main.js', gulp.parallel(scripts));
+}
+
